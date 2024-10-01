ef3029575f58ccf1da8149c33ae62f0265c55591
Radheykant <rbhardwa@gmail.com>
2024-10-01 11:23:14 +0530

    remove s3 conf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-variable "aws_region" {
-  description = "AWS account region."
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "environment" {
-  description = "Environment name."
-  type        = string
-  default     = "prod"
-}
-
+#variable "aws_region" {
+#  description = "AWS account region."
+#  type        = string
+#  default     = "us-east-1"
+#}
+#
+#variable "environment" {
+#  description = "Environment name."
+#  type        = string
+#  default     = "prod"
+#}
+#
 variable "ecs_cluster_ami_name" {
   description = "AMI name for the ECS host."
   type        = string
