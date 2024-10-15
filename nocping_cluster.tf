6707e48475c3a6305e3150548582cd6f0f5b1185
Radheykant <rbhardwa@gmail.com>
2024-10-15 14:30:49 +0530

    fix assignment of public ip

--- nocping_cluster.tf
+++ nocping_cluster.tf
@@ -15,10 +15,10 @@ resource "aws_launch_template" "ecs_ec2" {
   image_id               = data.aws_ssm_parameter.ecs_node_ami.value
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ecs_node_sg.id]
- # network_interfaces {
- #   associate_public_ip_address = false  # Disable auto public IP assignment
- #   security_groups = [aws_security_group.ecs_node_sg.id]
- # }
+  network_interfaces {
+     associate_public_ip_address = false  # Disable auto public IP assignment
+     security_groups = [aws_security_group.ecs_node_sg.id]
+  }
 
   iam_instance_profile { arn = aws_iam_instance_profile.ecs_node.arn }
   monitoring { enabled = true }
