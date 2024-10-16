d5136348db380d6d518b6a00bd74b7a02482b4ab
Radheykant <rbhardwa@gmail.com>
2024-10-16 18:26:51 +0530

    update nocping svc and sg

--- sg.tf
+++ sg.tf
@@ -5,17 +5,17 @@ resource "aws_security_group" "ecs_node_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
   
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"] 
   }
@@ -30,20 +30,20 @@ resource "aws_security_group" "ecs_task" {
 
 # This ingress rule allow the all traffic from internet bcoz we are not using ALB
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }  
 
-#  ingress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = [aws_vpc.main.cidr_block]
-#    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-#  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
 
   egress {
     from_port   = 0
