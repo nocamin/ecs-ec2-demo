b99c0a008d7316341482fc7c291378928976350c
Radheykant <rbhardwa@gmail.com>
2024-11-07 18:09:31 +0530

    module used for 7 regions

--- main.tf
+++ main.tf
@@ -1,10 +1,39 @@
-terraform {
-  required_providers {
-    aws = { source = "hashicorp/aws", version = "5.17.0" }
-  }
+variable "regions" {
+  type    = list(string)
+  default = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "eu-west-1", "ap-southeast-1", "ap-northeast-1"]
 }
 
-provider "aws" {
-# profile = "default"
-  region  = "us-east-1"
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account ID"
+  default     = "147997118683"
+}
+
+variable "environment" {
+  description = "Environment name."
+  type        = string
+  default     = "dev"
+}
+
+variable "icinga_cctld_au_epp_user" {
+  description = "The Icinga Testing CCTLD.AU EPP Username"
+  type        = string
+}
+
+variable "icinga_cctld_au_epp_password" {
+  description = "The Icinga Testing CCTLD.AU EPP Password"
+  type        = string
+  sensitive   = true
+}
+
+module "ecs_cluster" {
+  for_each = { for region in var.regions : region => region }
+
+  source = "./modules/nocping_ecs"
+
+  aws_account_id  = var.aws_account_id
+  aws_region       = each.key
+  environment      = var.environment
+  icinga_cctld_au_epp_user    = var.icinga_cctld_au_epp_user
+  icinga_cctld_au_epp_password = var.icinga_cctld_au_epp_password
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-variable "icinga_cctld_au_epp_user" {
-  description = "The Icinga Testing CCTLD.AU EPP Username"
-  type        = string
-}
-
-variable "icinga_cctld_au_epp_password" {
-  description = "The Icinga Testing CCTLD.AU EPP Password"
-  type        = string
-  sensitive   = true
-}
-
 variable "aws_account_id" {
   type        = string
   description = "AWS account id"
