864e65f74ae66fc2f93a044f161f8e1394dec858
Radheykant <rbhardwa@gmail.com>
2024-11-07 18:12:39 +0530

    remove variable error

--- modules/nocping_ecs/variables.tf
+++ modules/nocping_ecs/variables.tf
@@ -1,19 +1,3 @@
-variable "aws_account_id" {
-  type        = string
-  description = "AWS account ID"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "environment" {
-  description = "Environment name."
-  type        = string
-  default     = "dev"
-}
-
 variable "icinga_cctld_au_epp_user" {
   description = "The Icinga Testing CCTLD.AU EPP Username"
   type        = string
