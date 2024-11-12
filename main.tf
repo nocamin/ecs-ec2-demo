0763130c43d6ce8cb7dbcc9f91aa807470c49d01
Radheykant <rbhardwa@gmail.com>
2024-11-12 13:28:45 +0530

    test

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "ecs_us_east_1" {
-  source        = "./modules/nocping_ecs"
-  region        = "us-east-1"
-  default_region = var.default_region
-  icinga_cctld_au_epp_user    = var.icinga_cctld_au_epp_user
-  icinga_cctld_au_epp_password = var.icinga_cctld_au_epp_password
-  providers = {
-    aws = aws.us-east-1
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "5.17.0" }
   }
+}
+
+provider "aws" {
+# profile = "default"
+  region  = "us-east-1"
 }
