c95dd3319aea0bdff3e9c87431cdda7813d16cab
Radheykant <rbhardwa@gmail.com>
2024-11-12 17:39:50 +0530

    update provider block

--- main.tf
+++ main.tf
@@ -2,15 +2,18 @@ module "ecs_cluster_us_east_1" {
   source                          = "./modules/ecs_cluster"
   icinga_cctld_au_epp_user        = var.icinga_cctld_au_epp_user
   icinga_cctld_au_epp_password    = var.icinga_cctld_au_epp_password
-  providers = { aws = aws.us-east-1 }
+  providers = {
+    aws = aws.us-east-1
+  }
 }
 
 module "ecs_cluster_us_west_2" {
   source                         = "./modules/ecs_cluster"
   icinga_cctld_au_epp_user       = var.icinga_cctld_au_epp_user
   icinga_cctld_au_epp_password   = var.icinga_cctld_au_epp_password
-
-  providers = { aws = aws.us-west-2 }
+  providers = {
+    aws = aws.us-west-2
+  }
 }
 
 
