4d905df8bcfd10931b22cb9bedc4df3f160e0832
Radheykant <rbhardwa@gmail.com>
2024-11-12 16:54:18 +0530

    fix

--- modules/ecs_cluster/nocping_cluster.tf
+++ modules/ecs_cluster/nocping_cluster.tf
@@ -1,3 +1,15 @@
+module "networking" {
+  source = "../networking"
+}
+
+module "global" {
+  source = "../global"
+}
+
+resource "aws_security_group" "ecs_task" {
+  vpc_id = module.networking.vpc_id
+}
+
 # --- ECS Cluster ---
 
 resource "aws_ecs_cluster" "main" {
