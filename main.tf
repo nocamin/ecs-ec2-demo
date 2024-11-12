899fed9307fce881bb7ae916592bcc412bc71d22
Radheykant <rbhardwa@gmail.com>
2024-11-12 13:21:01 +0530

    update provider and main

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-variable "regions" {
-  type    = list(string)
-  default = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "eu-west-1", "ap-southeast-1", "ap-northeast-1"]
-}
-
-
-module "ecs_cluster" {
-
-  #for_each = { for region in var.regions : region => region }
-
-  source = "./modules/nocping_ecs"
-
-  aws_account_id  = var.aws_account_id
-  aws_region       = each.key
-  environment      = var.environment
+module "ecs_us_east_1" {
+  source        = "./modules/nocping_ecs"
+  region        = "us-east-1"
+  default_region = var.default_region
   icinga_cctld_au_epp_user    = var.icinga_cctld_au_epp_user
   icinga_cctld_au_epp_password = var.icinga_cctld_au_epp_password
+  providers = {
+    aws = aws.us-east-1
+  }
 }
--- providers.tf
+++ providers.tf
@@ -1,3 +1,5 @@
+#providers.tf
+
 terraform {
   required_providers {
     aws = {
@@ -6,3 +8,18 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  region = var.default_region
+}
+
