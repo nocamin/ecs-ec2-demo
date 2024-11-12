be3f865993f7cd4a70867c3bab95d7c9f2d64436
Radheykant <rbhardwa@gmail.com>
2024-11-12 16:47:05 +0530

    fix call variable

--- main.tf
+++ main.tf
@@ -7,15 +7,21 @@ module "networking" {
 
 module "ecs_cluster_us_east_1" {
   source    = "./modules/ecs_cluster"
-  icinga_cctld_au_epp_user     = var.icinga_cctld_au_epp_user
-  icinga_cctld_au_epp_password = var.icinga_cctld_au_epp_password
+  vpc_id                          = aws_vpc.main.id
+  s3_bucket_nocping               = aws_s3_bucket.nocping.bucket
+  icinga_cctld_au_epp_user        = var.icinga_cctld_au_epp_user
+  icinga_cctld_au_epp_password    = var.icinga_cctld_au_epp_password
+  
   providers = { aws = aws.us-east-1 }
 }
 
 module "ecs_cluster_us_west_2" {
   source    = "./modules/ecs_cluster"
-  icinga_cctld_au_epp_user     = var.icinga_cctld_au_epp_user
-  icinga_cctld_au_epp_password = var.icinga_cctld_au_epp_password
+  vpc_id                         = aws_vpc.main.id
+  s3_bucket_nocping              = aws_s3_bucket.nocping.bucket
+  icinga_cctld_au_epp_user       = var.icinga_cctld_au_epp_user
+  icinga_cctld_au_epp_password   = var.icinga_cctld_au_epp_password
+
   providers = { aws = aws.us-west-2 }
 }
 
--- modules/ecs_cluster/variables.tf
+++ modules/ecs_cluster/variables.tf
@@ -9,6 +9,16 @@ variable "icinga_cctld_au_epp_password" {
   sensitive   = true
 }
 
+variable "vpc_id" {
+  description = "ID of the VPC to associate with ECS resources"
+  type        = string
+}
+
+variable "s3_bucket_nocping" {
+  description = "S3 bucket name for nocping certs"
+  type        = string
+}
+
 variable "aws_account_id" {
   type        = string
   description = "AWS account id"
