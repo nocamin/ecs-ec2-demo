cea8b0d05a3b16fe299aebe4de34d08645f63d3a
Radheykant <rbhardwa@gmail.com>
2024-11-13 17:58:31 +0530

    Absolute provider change

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "ecs_cluster_us_east_1" {
   source                          = "./modules/ecs_cluster"
+  region                          = "us-east-1"
   icinga_cctld_au_epp_user        = var.icinga_cctld_au_epp_user
   icinga_cctld_au_epp_password    = var.icinga_cctld_au_epp_password
   providers = {
@@ -9,6 +10,7 @@ module "ecs_cluster_us_east_1" {
 
 module "ecs_cluster_us_west_2" {
   source                         = "./modules/ecs_cluster"
+  region                         = "us-west-2"
   icinga_cctld_au_epp_user       = var.icinga_cctld_au_epp_user
   icinga_cctld_au_epp_password   = var.icinga_cctld_au_epp_password
   providers = {
