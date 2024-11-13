df3b2ce9c2eaeff823ed4b639c040563a8bceea0
Radheykant <rbhardwa@gmail.com>
2024-11-13 19:26:34 +0530

    fix s3 bucket

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "ecs_cluster_us_west_2" {
   source                         = "./modules/ecs_cluster"
   icinga_cctld_au_epp_user       = var.icinga_cctld_au_epp_user
   icinga_cctld_au_epp_password   = var.icinga_cctld_au_epp_password
+  bucket_region                  = var.bucket_region
   providers = {
     aws = aws.us_west_2
   }
