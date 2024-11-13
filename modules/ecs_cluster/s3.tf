1b1fbcb1ef6fcea89477751f1155db9cd96134e3
Radheykant <rbhardwa@gmail.com>
2024-11-13 18:30:55 +0530

    fix s3

---
+++ modules/ecs_cluster/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "nocping" {
+  bucket = "${var.aws_account_id}-${var.aws_region}-observability-apps"
+  count  = var.region == var.aws_region ? 1 : 0
+  tags = {
+    Name        = "nocping"
+    Environment = "Dev"
+  }
+}
+
+#resource "aws_s3_bucket_acl" "my_bucket_acl" {
+#  bucket = aws_s3_bucket.nocping.id
+#  acl    = "private"
+#}
+
+#resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+#  bucket = aws_s3_bucket.my_bucket.id
+#
+#  versioning_configuration {
+#    enabled = true
+#  }
+#}
